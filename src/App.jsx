a6d2791892a9df09aab5a64781516a48806dd4de
hirwapatrick <166864341+hirwapatrick@users.noreply.github.com>
2025-07-04 20:23:19 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 import Favorites from "./pages/Favorites";
-import "bootstrap/dist/css/bootstrap.min.css";
 import AdminApp from "./admin/AdminApp";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <>
-      {" "}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetails />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/admin" element={<AdminApp />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/admin" element={<AdminApp />} />
+        
+        {/* Catch-all route for unknown paths */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 };
 
