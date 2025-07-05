6ed56142172d2718993f474eaacbad027900cc47
hirwapatrick <166864341+hirwapatrick@users.noreply.github.com>
2025-07-05 20:20:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        sitemap: './public/sitemap.xml',
-      },
-    },
-  },
-});
+})
+
