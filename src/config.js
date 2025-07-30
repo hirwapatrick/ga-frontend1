2c9d1121ba8654daf926157f78ff4b66016ffa1b
hirwapatrick <166864341+hirwapatrick@users.noreply.github.com>
2025-07-30 12:32:08 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// src/config.js
-export const API_BASE = "https://getagasobanuye.onrender.com?419acf01286331ef44556716ed36f28a512494af4864ea7a0c21b56201f778f9";
+export const API_BASE = "https://getagasobanuye.onrender.com";
+export const API_KEY = "419acf01286331ef44556716ed36f28a512494af4864ea7a0c21b56201f778f9";
