b174a775316dcc15c9d8ad50335a2ad0c5b5afff
hirwapatrick <166864341+hirwapatrick@users.noreply.github.com>
2025-07-30 14:08:21 +0200

    Update AdminDashboard.jsx

--- src/admin/AdminDashboard.jsx
+++ src/admin/AdminDashboard.jsx
@@ -87,6 +87,9 @@ export default function AdminDashboard() {
     setLoadingMovies(true);
     try {
       const res = await axios.get(`${API_BASE}/movies/admin`, {
+        headers: {
+          'x-api-key': API_KEY
+        },
         params: { page: 1, limit: 10, q: search },
       });
       setMovies(res.data);
